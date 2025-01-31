8e7783b40cb5ffa0bc9837cb43702c784fcdae30
@Chinnannavar <yashchinnannavar18@gmail.com.>
2025-01-31 12:14:56 +0530

    all

--- src/User.jsx
+++ src/User.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const User = ({ name, age, email }) => {
   return (
-    <div style={{ border: '1px solid black', padding: '10px', margin: '10px', width: '300px', height: '200px', backgroundColor: 'lightgray', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' }}>
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px', width: '300px', height: '200px', backgroundColor: 'lightgray', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' , color:'red' }}>
       <h2 >Name: {name}</h2>
       <h2>Age: {age}</h2>
       <h2>Email: {email}</h2>
